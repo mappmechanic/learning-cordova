a6c3ceab331e1fc10f3b482ea0a8a22ddfba6dc8
Rahat Khanna <rahat.khanna@flipkart.com>
2016-06-23 18:24:56 +0530

    Added SplashScreens with the Plugins

--- hello/platforms/android/platform_www/cordova_plugins.js
+++ hello/platforms/android/platform_www/cordova_plugins.js
@@ -1,9 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-splashscreen": "3.2.2"
 };
 // BOTTOM OF METADATA
 });
