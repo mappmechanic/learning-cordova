307318d17176f99a2bcc98d0ae5428a6b706ccba
Rahat Khanna <rahat.khanna@flipkart.com>
2016-07-02 16:13:51 +0530

    Implemented Cordova Contacts Plugin

--- hello/www/js/plugins/contacts.js
+++ hello/www/js/plugins/contacts.js
@@ -1,19 +1,30 @@
-function contacts(){
-	document.addEventListener('deviceready',onDeviceReady,false);
-	function onDeviceReady(){
-		console.log('Device is Ready');
-		window.pickAContact = pickAContact;
 
-		function pickAContact() {
-			navigator.contacts.pickContacts(successCallback,errorCallback);
+app.on({page: 'contacts', preventClose: false, content: 'contacts.html', readyDelay: 1}, function(activity) {
+	var onAction = function(evt) {
+        var target = evt.target;
 
-			function successCallback(contact){
-				$('#selected_contact').html('Selected Contact: <br>'+JSON.stringify());
-			}
+        if(target.getAttribute('data-order') === 'pickAContact') {
+            deviceReady(function(){
+				navigator.contacts.pickContact(successCallback,errorCallback);
 
-			function errorCallback(message){
-				alert('Error Occurred: '+message);
-			}
-		}
-	}
-}
+				function successCallback(contact){
+					$('#selected_contact').removeClass('hidden');
+					$('#display_name').html(contact["displayName"]);
+					$('#mobile_number').html(contact.phoneNumbers && contact.phoneNumbers[0] ? contact.phoneNumbers[0].value : 'N/A');
+					$('#email').html(contact.email && contact.email[0] ? contact.email[0].value : 'N/A');
+					$('#organisation').html(contact.organizations && contact.organizations[0] ? contact.organizations[0].name : 'N/A');
+				}
+
+				function errorCallback(message){
+					alert('Error Occurred: '+message);
+				}
+			});
+        } else {
+            phonon.alert('Your order has been canceled.', 'Dear customer');
+        }
+    };
+
+	activity.onCreate(function() {
+		document.getElementById('pickContactBtn').on('tap',onAction);
+    });
+});
